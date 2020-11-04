8cc4fae379ea5b8aeb1794d54440ffddd3441ea6
Felipe Cardoso <felipecrdos@gmail.com>
2020-11-04 01:10:14 -0300

    not allowed to save and delete to database

--- routes/AsteroidRoute.js
+++ routes/AsteroidRoute.js
@@ -40,15 +40,16 @@ route.get('/', async (req, res)=>{
 // POST
 route.post('/save', async (req, res)=>{
     try{
-        const asteroid = await AsteroidModel.create({
-            _id:req.body._id,
-            name:req.body.name,
-            diameter:req.body.diameter,
-            sum_distance:req.body.sum_distance,
-            discovery_date:req.body.discovery_date,
-            finder:req.body.finder
-        })
-        res.json(asteroid)
+        // const asteroid = await AsteroidModel.create({
+        //     _id:req.body._id,
+        //     name:req.body.name,
+        //     diameter:req.body.diameter,
+        //     sum_distance:req.body.sum_distance,
+        //     discovery_date:req.body.discovery_date,
+        //     finder:req.body.finder
+        // })
+        // res.json(asteroid)
+        res.json({message:"SAVED" ,asteroid: req.body})
     }
     catch(err){
         res.json({message:err})
@@ -75,8 +76,10 @@ route.put('/update/:id', async (req, res)=>{
 // DELETE BY ID
 route.delete('/delete/:id', async (req, res)=>{
     try{
-        const asteroid = await AsteroidModel.deleteOne({_id:req.params.id})
-        res.json(asteroid)
+        // const asteroid = await AsteroidModel.deleteOne({_id:req.params.id})
+        // res.json(asteroid)
+        const asteroid = await AsteroidModel.findById(req.params.id)
+        res.json({message:"DELETED!",asteroid:asteroid})
     }
     catch(err){
         res.json({message:err})
