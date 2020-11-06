50acd960b52769c096a7b6631d0347f79ad5dd63
Felipe Cardoso <felipecrdos@gmail.com>
2020-11-06 14:07:33 -0300

    app set json spaces, 4

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.set('json spaces', 4)
 
 app.use('/createsomedata', async (req, res, next)=>{
     //util.createSomeData()
